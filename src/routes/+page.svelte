0948eb6e5260a500112e88dae7b1c1acd4ee0692
darkhacker1906 <singhabhinandan634@gmail.com>
2025-05-16 11:29:46 +0530

    Added the sass

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Welcome to my spotify application</h1>
-
+<style lang="scss">
+    @use '../styles/functions';
+    @use '@unsass/breakpoint';
+    h1{
+        color:red;
+        font-size: functions.toRem(36);
+         @include breakpoint.up("md") {
+        color: darkcyan;
+    }
+     @include breakpoint.up("lg") {
+        color: green;
+    }
+    }
+</style>
