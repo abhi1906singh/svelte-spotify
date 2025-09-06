b952ad9de0eb84fd590c821a2fc9443dad3e6545
darkhacker1906 <singhabhinandan634@gmail.com>
2025-09-06 19:07:00 +0530

    Added the navbar and fixed the deigns

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,17 @@
     import '../styles/main.scss';
 	import type { LayoutData } from './$types';
 	import LogoutButton from '$lib/components/LogoutButton.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
     	
 
     export let data:LayoutData;
     $: user=data.user;
 </script>
+<Navbar/>
 <slot/>
 {#if user}
+<div class="p-2">
 <div>Hello {data.user?.display_name}</div>
 <LogoutButton/>
+</div>
 {/if}
