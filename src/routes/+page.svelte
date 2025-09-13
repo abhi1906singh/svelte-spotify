780acd78527d730ab9820c1bbd7678a3289085c1
darkhacker1906 <singhabhinandan634@gmail.com>
2025-09-13 13:58:14 +0530

    Added the backend route for the search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,40 @@
+<script lang="ts">
+	let searchQuery = $state('');
+	let searchResults: Array = [];
+	async function handleSubmit(event: any) {
+		event.preventDefault();
+		let data = searchQuery.toLowerCase();
+
+		const response = await fetch(`/api/auth/search?q=${encodeURIComponent(data)}`);
+		if (response.ok) {
+			searchResults = await response.json();
+		}
+	}
+</script>
+
 <div class="p-2">
-<h1>Welcome to my spotify application</h1>
-<input/>
-<select>
-    <option>Hello</option>
-</select>
-<button>Button</button>
+	<h1>Welcome to my spotify application</h1>
+	<form onsubmit={handleSubmit}>
+		<input bind:value={searchQuery} />
+		<button type="submit">Search</button>
+	</form>
+	<select>
+		<option>Hello</option>
+	</select>
+	<button>Button</button>
 </div>
+
 <style lang="scss">
-    @use '../styles/functions';
-    @use '@unsass/breakpoint';
-    h1{
-        color:red;
-        font-size: functions.toRem(36);
-         @include breakpoint.up("md") {
-        color: darkcyan;
-    }
-     @include breakpoint.up("lg") {
-        color: green;
-    }
-    }
+	@use '../styles/functions';
+	@use '@unsass/breakpoint';
+	h1 {
+		color: red;
+		font-size: functions.toRem(36);
+		@include breakpoint.up('md') {
+			color: darkcyan;
+		}
+		@include breakpoint.up('lg') {
+			color: green;
+		}
+	}
 </style>
--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -1,11 +1,11 @@
-import { redirect } from "@sveltejs/kit";
-import { BASE_URL, SPOTIFY_APP_CLIENT_ID } from "$env/static/private";
-import pkce from "pkce-gen"
-import type { RequestHandler } from "./$types";
-import { codeVerifierMap,stateMap } from "$lib/server/codeVerifier";
+import { redirect } from '@sveltejs/kit';
+import { BASE_URL, SPOTIFY_APP_CLIENT_ID } from '$env/static/private';
+import pkce from 'pkce-gen';
+import type { RequestHandler } from './$types';
+import { codeVerifierMap, stateMap } from '$lib/server/codeVerifier';
 const scope =
 	'ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read';
-    const generateRandomString = (length: number) => {
+const generateRandomString = (length: number) => {
 	let randomString = '';
 	const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -15,20 +15,20 @@ const scope =
 	return randomString;
 };
 const state = generateRandomString(16);
-const challenge=pkce.create();
+const challenge = pkce.create();
 codeVerifierMap.set(state, challenge.code_verifier);
 stateMap.set(state, state);
-export const GET:RequestHandler=()=>{
-
-    throw redirect(303,`https://accounts.spotify.com/authorize?${
-        new URLSearchParams({
-            response_type:'code',
-            client_id:SPOTIFY_APP_CLIENT_ID,
-            scope:scope,
-            redirect_uri:`${BASE_URL}/api/auth/callback`,
-            state,
-            code_challenge_method:'S256',
-            code_challenge:challenge.code_challenge,
-        })
-    }`)
-}
+export const GET: RequestHandler = () => {
+	throw redirect(
+		303,
+		`https://accounts.spotify.com/authorize?${new URLSearchParams({
+			response_type: 'code',
+			client_id: SPOTIFY_APP_CLIENT_ID,
+			scope: scope,
+			redirect_uri: `${BASE_URL}/api/auth/callback`,
+			state,
+			code_challenge_method: 'S256',
+			code_challenge: challenge.code_challenge
+		})}`
+	);
+};
